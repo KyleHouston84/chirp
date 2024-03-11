d993389a246403f987d2e9515230ae5d5750e1b8
Kyle Houston <kylehouston@Kyles-MBP.localdomain>
2024-03-11 02:21:06 -0700

    Next image tag, post styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,23 @@
-import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from 'next/app'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api'
 
-import "~/styles/globals.css";
+import '~/styles/globals.css'
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <main className={`font-sans ${inter.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
