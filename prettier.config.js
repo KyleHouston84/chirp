7d535c0b2209c5fb70e236e1e720b0a9f82a713f
Kyle Houston <kylehouston@Kyles-MBP.localdomain>
2024-03-11 00:51:06 -0700

    Router

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,21 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false
 };
 
 export default config;
