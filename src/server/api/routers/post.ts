c8528b9bc31385199689083d67ed63c9e61b16dd
Kyle Houston <kylehouston@Kyles-MBP.localdomain>
2024-03-15 00:43:42 -0700

    Added rate limiting for posting

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,6 +3,9 @@ import { User } from '@clerk/nextjs/server';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
+import { Ratelimit } from '@upstash/ratelimit'; // for deno: see above
+import { Redis } from '@upstash/redis'; // see below for cloudflare and fastly adapters
+
 import {
   createTRPCRouter,
   privateProcedure,
@@ -12,6 +15,19 @@ import {
 const filterUserForClient = (user: User) => {
   return { id: user.id, username: user.username, imageUrl: user.imageUrl };
 };
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, '1 m'),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: '@upstash/ratelimit',
+});
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -50,6 +66,9 @@ export const postRouter = createTRPCRouter({
     .input(z.object({ content: z.string().emoji().min(1).max(280) }))
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
 
       const post = await ctx.db.post.create({
         data: {
