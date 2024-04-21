79829c25d2f1de2b2916337816d1e6f701c6c134
Kyle Houston <kylehouston@Kyles-MBP.localdomain>
2024-04-20 20:36:37 -0700

    Added more get post route to public

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,13 @@ import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/trpc/post.getAllOriginalPosts'],
+  publicRoutes: [
+    '/',
+    '/api/trpc/post.getAllOriginalPosts',
+    '/api/trpc/post.getById',
+    '/api/trpc/post.getRepliesById',
+    '/api/trpc/post.getPostsByUserId',
+  ],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route'],
